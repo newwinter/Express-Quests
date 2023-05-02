0afa8d2a3e6115f57e4ce33efd19edaa763ea351
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-05-02 09:19:34 +0200

    express quests finie

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
+const port = process.env.APP_PORT ?? 5001;
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+// const port = 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -22,3 +25,17 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+
+const users = require("./users");
+app.get("/api/users", users.getUsers);
+app.get("/api/users/:id", users.getUsersById);
+
+// app.get("/api/users/:id", (req, res) => {
+//   const question = questions.find((quest) => quest.id === +req.params.id);
+//   if (question) {
+//     res.status(200).json(question);
+//   } else {
+//     res.status(404).send("Not Found");
+//   }
+// });
